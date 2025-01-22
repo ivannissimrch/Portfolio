8b8c6816f134a3f04f5a3cf1e485b92660d4ad19
ivannissimrch <ivannissimrch@gmail.com>
2025-01-22 10:11:03 -0500

    Add font familly and font sizes classes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter, Onest } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -8,6 +8,16 @@ const geistSans = Geist({
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const inter = Inter({
+  variable: "--font-Inter",
+  subsets: ["latin"],
+});
+
+const onest = Onest({
+  variable: "--font-Onest",
   subsets: ["latin"],
 });
 
