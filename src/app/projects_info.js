8c377deab867633d9e280c3ddd610ba54df7c011
ivannissimrch <ivannissimrch@gmail.com>
2025-04-16 05:49:01 -0400

    Add descriptions to projects

--- src/app/projects_info.js
+++ src/app/projects_info.js
@@ -2,7 +2,7 @@ export const PROJECTS_INFO = [
   {
     title: "WeeklyBytes",
     description:
-      "A meal Scheduler App tlfklsdkflsdkf lksdlfk sdlfksdlkflsdkfls d lsdklfksdlfklsdkf sd ;lksdfl sdklf dsk sd;lkf;lskf sd;fl ksd;l fks;lfsk;lsfdkfsdl; k",
+      "Weekly Bytes is an intuitive web application designed to help managers effortlessly create and share weekly meal plans while prioritizing staff safety and dietary needs.",
     image: "/images/weeklyBytesMobile.gif",
     imageDesktop: "/images/weeklyBytesDesktop.gif",
     teamWorkMobileImage: "/images/weeklyBytesMTeamWork.png",
@@ -15,17 +15,17 @@ export const PROJECTS_INFO = [
       "/images/html-icon.svg",
       "/images/tailwind-icon.svg",
     ],
-    purpose: `Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.`,
+    purpose: `application designed to help managers effortlessly create and share weekly meal plans while prioritizing staff safety and dietary needs. With a vast selection of delicious meals, the app automatically filters out any dishes containing allergens identified by staff members. It then generates a randomized menu—ensuring variety and eliminating meal repetition within the same week.`,
 
-    techStack: `Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.`,
+    techStack: `We used React, React router for routing, Tailwind for styling, Material UI for the alert messages and react-select library to create the dropdown menus `,
 
-    collaboration: `Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.`,
+    collaboration: `I collaborated at Chingu during six weeks with a group of 9 people, 5 developers, 1 product owner, 1 UX designer, 1 scrum Master and 1 voyage guide`,
     thoughtProcess: `Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.`,
   },
   {
     title: "Stocks",
     description:
-      "Applioidsjdos opsdjoisjdoijs;dfklfdkdfldfkldk;lfdk;lfdkfd;lkfd;lk;lfdkdfl;kd",
+      "A stock application that allows you to search for and retrieve data about stocks.",
     image: "/images/stocksMobile.gif",
     imageDesktop: "/images/stocksDesktop.gif",
     teamWorkMobileImage: "/images/stocksMTeamWork.png",
@@ -49,7 +49,7 @@ export const PROJECTS_INFO = [
   {
     title: "Slice",
     description:
-      "A user-friendly expense splitting application that takes the headache  A user-friendly expense splitting application that takes the headache out of shared finances.A user-friendly expense splitting application that takes the headache out of shared finances.",
+      "Expense-splitting app tailored specifically for roommates and friends. Slice helps users manage and divide expenses such as rent, utilities, subscription services, groceries, and more",
     image: "/images/sliceMobile.gif",
     imageDesktop: "/images/sliceDesktop.gif",
     teamWorkMobileImage: "/images/sliceMTeamWork.png",
@@ -62,7 +62,7 @@ export const PROJECTS_INFO = [
       "/images/html-icon.svg",
       "/images/tailwind-icon.svg",
     ],
-    purpose: `Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.`,
+    purpose: `nceptos himenaeos.`,
 
     techStack: `Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.`,
 
@@ -71,7 +71,8 @@ export const PROJECTS_INFO = [
   },
   {
     title: "ComingSoon",
-    description: "",
+    description:
+      "Ember AI is an assistant app designed to help a user craft a well written, complete prompt to use when utilizing an AI platform in seeking information.",
     image: "/images/comingSoon.jpg",
     imageDesktop: "/images/comingSoon.jpg",
     date: "2024",
